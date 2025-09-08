a6edf05cc8763c2a4f3b4ce3744ce8b7b7e04ff5
fanok3000 <fanok3000@outlook.com>
2025-09-08 11:15:05 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,11 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_6us2pYayA', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '266i730kcrrn2o4kl1ieseckbv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
